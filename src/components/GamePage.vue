3c4a570b44b09365127b7451f919ec3e3fc412ed
kimdj <kimdj@uitgis.com>
2024-03-22 16:26:20 +0900

    알 깨기 게임 추가

--- src/components/GamePage.vue
+++ src/components/GamePage.vue
@@ -2,45 +2,58 @@
 import { ref, onMounted } from "vue";
 import { pageConfig } from "@/store";
 import { gsap } from "gsap";
+import Draggable from "gsap/Draggable";
 
 const store = pageConfig();
 
+const gaugeValue = ref(0);
+const gaugeMax = 100;
+const gaugeDecreaseRate = 10; // 10 units per second
+
 const imageSource = ref(require("@/images/along1.png"));
 const imageSource2 = ref(require("@/images/along1.png"));
+const imageSource3 = ref(require("@/images/along1.png"));
 
-const isMoving = ref(true);
+const eggGame = ref(null);
 
 const animatedImage = ref(null);
-let timeline = gsap.timeline();
+
+const shakeIt = () => {
+  if (gaugeValue.value < 100) {
+    gameTimeLine.restart();
+    gaugeValue.value = Math.min(gaugeValue.value + 10, gaugeMax);
+  }
+
+  if (gaugeValue.value >= 100) {
+    imageSource.value = require("@/images/along2.png");
+  }
+};
+
+let gameTimeLine = gsap.timeline();
+let timeline1 = gsap.timeline();
+
+// eslint-disable-next-line no-unused-vars
+let timeline2 = gsap.timeline();
 
 const vw = window.innerWidth / 100;
 const target = vw * 95 - 300;
 
-// const isMoving2 = ref(true);
-function shakeIt() {
-  imageSource.value = require("@/images/test.gif");
-  isMoving.value = false;
-}
-function stopShakeIt() {
-  imageSource.value = require("@/images/along1.png");
-  isMoving.value = true;
-}
 const { changePage } = store;
 
 // 애니메이션 멈추기
 const pauseAnimation = () => {
   imageSource2.value = require("@/images/test2.gif");
-  timeline.pause();
+  timeline1.pause();
 };
 // 애니메이션 재시작
 const resumeAnimation = () => {
   imageSource2.value = require("@/images/along1.png");
-  timeline.resume();
+  timeline1.resume();
 };
 
 onMounted(() => {
   changePage("미니게임");
-  timeline = gsap.to(animatedImage.value, {
+  timeline1 = gsap.to(animatedImage.value, {
     x: target,
     yoyo: true,
     repeat: -1,
@@ -48,18 +61,56 @@ onMounted(() => {
     ease: "power1.inOut",
     paused: false,
   });
+
+  timeline2 = gsap.to(animatedImage.value, {
+    y: "30",
+    repeat: -1,
+    duration: 0.8,
+    yoyo: true,
+    ease: "power1.inOut",
+  });
+
+  gsap.registerPlugin(Draggable);
+  Draggable.create("#draggableTest", {
+    bounds: window,
+    onClick: function (event) {
+      console.log("윙크!", event);
+    },
+  });
+
+  gameTimeLine
+    .to(eggGame.value, { rotation: 3, duration: 0.05 })
+    .to(eggGame.value, { rotation: -3, duration: 0.05 })
+    .to(eggGame.value, { rotation: 3, duration: 0.05 })
+    .to(eggGame.value, { rotation: -3, duration: 0.05 })
+    .to(eggGame.value, { rotation: 0, duration: 0.05 })
+    .pause();
 });
+
+setInterval(() => {
+  if (gaugeValue.value < 100) {
+    gaugeValue.value = Math.max(gaugeValue.value - gaugeDecreaseRate, 0);
+  }
+}, 1000); //
 </script>
 
 <template>
   <a-typography-title :level="2">미니게임</a-typography-title>
   <img
+    ref="eggGame"
     :src="imageSource"
     style="width: 300px; object-fit: contain"
-    @mouseover="shakeIt"
-    @mouseout="stopShakeIt"
-    :class="{ 'shake-animation': isMoving }"
+    @click="shakeIt"
   />
+  <div style="height: 20px; width: 100px; border: 1px solid black">
+    <div
+      :style="{
+        height: '100%',
+        width: gaugeValue + '%',
+        backgroundColor: 'blue',
+      }"
+    ></div>
+  </div>
   <div style="width: 100%">
     <img
       ref="animatedImage"
@@ -69,31 +120,10 @@ onMounted(() => {
       @mouseup="resumeAnimation"
     />
   </div>
+
+  <img
+    id="draggableTest"
+    :src="imageSource3"
+    style="width: 80px; object-fit: contain"
+  />
 </template>
-
-<style>
-@keyframes shake {
-  0% {
-    transform: rotate(0deg);
-  }
-  20% {
-    transform: rotate(-1deg);
-  }
-  40% {
-    transform: rotate(1deg);
-  }
-  60% {
-    transform: rotate(-1deg);
-  }
-  80% {
-    transform: rotate(1deg);
-  }
-  100% {
-    transform: rotate(0deg);
-  }
-}
-
-.shake-animation {
-  animation: shake 0.15s ease-in-out infinite;
-}
-</style>
