3e0005e2c60f9dead466a250246a2048daaa196c
kimdj <kimdj@uitgis.com>
2024-03-25 09:54:36 +0900

    에피소드 갤러리 틀 작업 완료

--- src/components/EpisodePage.vue
+++ src/components/EpisodePage.vue
@@ -1,43 +1,71 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, onUnmounted, computed } from "vue";
 import { pageConfig } from "@/store";
 
 const store = pageConfig();
 const { changePage } = store;
 
+const imageSources = [
+  require("@/images/along1.png"),
+  require("@/images/along2.png"),
+  require("@/images/test.gif"),
+  require("@/images/test2.gif"),
+  require("@/images/along1.png"),
+  require("@/images/along2.png"),
+  require("@/images/test.gif"),
+  require("@/images/test2.gif"),
+  require("@/images/test2.gif"),
+  require("@/images/test2.gif"),
+  require("@/images/along2.png"),
+  require("@/images/along2.png"),
+];
 
-const image1 = ref(require('@/images/along1.png'));
-const image2 = ref(require('@/images/test.gif'));
+let screenWidth = ref(window.innerWidth);
 
+const getImagesForColumn = (colNumber) => {
+  return imageSources.filter(
+    (_, index) => index % colCounts.value === colNumber - 1
+  );
+};
 
+const handleResize = () => {
+  screenWidth.value = window.innerWidth;
+};
+
+onUnmounted(() => {
+  window.removeEventListener("resize", handleResize);
+});
+
+const colCounts = computed(() => {
+  if (screenWidth.value >= 1200) {
+    return 6;
+  } else if (screenWidth.value >= 992) {
+    return 4;
+  } else {
+    return 2;
+  }
+});
 
 onMounted(() => {
   changePage("에피소드");
+  window.addEventListener("resize", handleResize);
 });
 </script>
 
 <template>
   <a-row>
-    <a-col :span="4">
-      <img :src="image1" style="width: 100%; object-fit: contain;"/>
-      <img :src="image1" style="width: 100%; object-fit: contain;"/>
+    <a-col
+      :xs="12"
+      :lg="{ span: 24 / colCounts }"
+      v-for="n in colCounts"
+      :key="`col-${n}`"
+    >
+      <img
+        v-for="(src, index) in getImagesForColumn(n)"
+        :src="src"
+        :key="`img-${index}`"
+        style="width: 100%; object-fit: contain"
+      />
     </a-col>
-    <a-col :span="4">
-      <img :src="image2" style="width: 100%; object-fit: contain;"/>
-      <img :src="image1" style="width: 100%; object-fit: contain;"/>
-    </a-col>
-    <a-col :span="4">
-      <img :src="image1" style="width: 100%; object-fit: contain;"/>
-    </a-col>
-    <a-col :span="4">
-      <img :src="image1" style="width: 100%; object-fit: contain;"/>
-    </a-col>
-    <a-col :span="4">
-      <img :src="image1" style="width: 100%; object-fit: contain;"/>
-    </a-col>
-    <a-col :span="4">
-      <img :src="image1" style="width: 100%; object-fit: contain;"/>
-    </a-col>
-
   </a-row>
 </template>
